6777af677823e5a740b28d8661acd612d127302a
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-07-02 18:22:56 +0200

    update axios api

--- js/app.js
+++ js/app.js
@@ -19,16 +19,14 @@ const app = new Vue({
         */
 
         //Questo ci permette di interrompere subito il ciclo in caso il server restituisca un errore
-        for(let i = 0; i < this.qty; i++){
+        mails:for(let i = 0; i < this.qty; i++){
             try{
-               await axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(result => {
-                    this.emails.push(result.data.response);
-                })
+               let response = await axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail');
+               this.emails.push(response.data.response);
             }catch(e){
                 this.error = e;
-                break;
+                break mails;
             }
         }
 
